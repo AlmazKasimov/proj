6e5273709b6d9606941cbd002f23f8fe4c5952b5
Almaz Kasimov <git.user.crimsonri@mail.ru>
2024-09-02 23:15:24 +0300

    улучшили логику

--- src/main/java/com/stepup/proj/Main/MainApplication.java
+++ src/main/java/com/stepup/proj/Main/MainApplication.java
@@ -2,7 +2,15 @@ package com.stepup.proj.Main;
 
 public class MainApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-        System.out.println("Almaz +++");
+        System.out.println("number 23");
+        System.out.println("Master QA");
+    }
+
+    public static void doSomething()
+    {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(4);
+        System.out.println(4);
     }
 }
