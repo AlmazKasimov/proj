7e0b1f21f88afeb1304f5a5507f8b92be7ae687d
Almaz Kasimov <git.user.crimsonri@mail.ru>
2024-09-02 23:11:22 +0300

    добавлен вывод сообщения main и doSomething

--- src/main/java/com/stepup/proj/Main/MainApplication.java
+++ src/main/java/com/stepup/proj/Main/MainApplication.java
@@ -3,6 +3,15 @@ package com.stepup.proj.Main;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Almaz +++");
+        System.out.println("----");
+        System.out.println("plus");
+    }
+
+    public static void doSomething()
+    {
+        System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
+        System.out.println(4);
     }
 }
