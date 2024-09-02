82f70021f51e90111676eb1ee1155ae0dd848ab7
Almaz Kasimov <git.user.crimsonri@mail.ru>
2024-09-02 22:58:56 +0300

    доработка логики проекта

--- src/main/java/com/stepup/proj/Main/MainApplication.java
+++ src/main/java/com/stepup/proj/Main/MainApplication.java
@@ -3,6 +3,6 @@ package com.stepup.proj.Main;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Almaz ++++");
+        System.out.println("Almaz +++");
     }
 }
